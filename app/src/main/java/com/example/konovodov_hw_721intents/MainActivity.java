00d0f989eda25a1e33d132da0e7bb153e5642f9f
VladimirKonovodov <kon_vlad@mail.ru>
2020-06-15 02:51:22 +0500

    new correct

--- app/src/main/java/com/example/konovodov_hw_721intents/MainActivity.java
+++ app/src/main/java/com/example/konovodov_hw_721intents/MainActivity.java
@@ -6,20 +6,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button searchButton;
-    EditText searchLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        searchButton = findViewById(R.id.searchButton);
-        searchLine = findViewById(R.id.searchLine);
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        final EditText searchLine = findViewById(R.id.searchLine);
+        findViewById(R.id.searchButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String searchData = searchLine.getText().toString();
